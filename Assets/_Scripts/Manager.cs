1239c556a19016fb4edf20ce5198d1ff6f6ab18c
mrome007 <mrome007@ucr.edu>
2014-10-16 17:05:27 -0700

    Changing Modes
    Changes Modes from Buying to Shooting and vice versa via button press

---
+++ Assets/_Scripts/Manager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+	//instance of the singleton.
+	private SingletonForMode SM;
+	// Use this for initialization
+	void Start () 
+	{
+		SM = SingletonForMode.GetInstance ();
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(600f,300f,200f,50f),"CHANGE MODE"))
+		{
+			SM.ChangeMode();
+			Debug.Log(SingletonForMode.Play_Mode);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/_Scripts/SingletonForMode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonForMode
+{
+	public enum PlayMode
+	{
+		BUYmode,
+		SHOOTmode
+	}
+
+	public static PlayMode Play_Mode;
+	private static SingletonForMode instance = null;
+	private SingletonForMode()
+	{
+		Play_Mode = PlayMode.BUYmode;
+	}
+
+	public static SingletonForMode GetInstance()
+	{
+		if(instance == null)
+			instance = new SingletonForMode();
+		return instance;
+	}
+
+	public void ChangeMode()
+	{
+		if(Play_Mode == PlayMode.BUYmode)
+			Play_Mode = PlayMode.SHOOTmode;
+		else
+			Play_Mode = PlayMode.BUYmode;
+	}
+}
