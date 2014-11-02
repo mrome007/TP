769e0ec50ca3de0cdab233fb1bcc89f92dcde272
mrome007 <mrome007@ucr.edu>
2014-11-01 20:59:20 -0700

    Added Shooting, will fix in future

---
+++ Assets/Scripts/FireTowerAmmo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTowerAmmo : MonoBehaviour 
+{
+	public const float ACCEL_GRAVITY = -0.02f;
+	public Vector3 dir;
+	public float mAmmoSpeed;
+	public float mAngle;
+	private float mElapsedTime;
+	// Use this for initialization
+	void Start ()
+	{
+		mAmmoSpeed = 100;
+		dir.y += Mathf.Sin(mAngle);
+		dir = dir.normalized;
+	}
+
+	void FixedUpdate ()
+	{
+		mElapsedTime += Time.deltaTime;
+		float xVelocity = dir.x * Time.deltaTime * mAmmoSpeed;
+		float yVelocity = dir.y * Time.deltaTime * mAmmoSpeed;
+		float zVelocity = dir.z * Time.deltaTime * mAmmoSpeed;
+		Vector3 translateVector = new Vector3(xVelocity, yVelocity, zVelocity);
+		transform.Translate (translateVector);
+		dir.y += ACCEL_GRAVITY;
+		if (gameObject) 
+		{
+			float yPosition = transform.position.y;
+			if (yPosition <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
--- Assets/Scripts/ShootMode.cs
+++ Assets/Scripts/ShootMode.cs
@@ -2,14 +2,58 @@
 using System.Collections;
 
 public class ShootMode : MonoBehaviour {
+	public GameObject TowerAmmo;
+	private Vector3 AmmoSpawn = new Vector3(0f,5.0f,-25f);
 
+	private const float MAX_FIRE_ANGLE = Mathf.PI / 2.0f;
+	private float mClick;
+	private float mRelease;
+	private Vector3 TargetPosition;
+	private GameObject TheTowerFire;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(SingletonManager.Play_Mode != SingletonManager.PlayMode.SHOOTmode)
+			return;
+		if(Input.GetMouseButtonDown(0))
+		{
+			mClick = Input.mousePosition.y;
+			Plane playerPlane = new Plane(Vector3.up, Vector3.zero);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float hitdist;
+			if(playerPlane.Raycast(ray, out hitdist))
+			{
+				TargetPosition = ray.GetPoint(hitdist);
+				Debug.Log(TargetPosition);
+			}
+
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			mRelease = Input.mousePosition.y;
+
+			float maxAngleScreenRatio = Screen.height / 2;
+
+			float deltaY = mRelease - mClick;
+			if (deltaY > maxAngleScreenRatio) 
+				deltaY = maxAngleScreenRatio; 
+			else if (deltaY < -maxAngleScreenRatio)
+				deltaY = -maxAngleScreenRatio;
+
+			float fireAngle = deltaY / maxAngleScreenRatio * MAX_FIRE_ANGLE;
+
+			TheTowerFire = (GameObject)Instantiate(TowerAmmo, AmmoSpawn, Quaternion.identity);
+			FireTowerAmmo fta = TheTowerFire.GetComponent<FireTowerAmmo>();
+			Vector3 dir = TargetPosition - AmmoSpawn;
+			if(Mathf.Sin(fireAngle) != 0)
+				dir.y -= dir.y * Mathf.Sin(fireAngle);
+			fta.dir = dir.normalized;
+			fta.mAngle = fireAngle;
+		}
 	}
 }
